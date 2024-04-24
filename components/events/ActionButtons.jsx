d5ca800dc4a588c7c22074f6ce807da0b8145a59
robiulsagor <robiul100.me@gmail.com>
2024-04-24 20:51:22 +0600

    added action button separate component and an edit

---
+++ components/events/ActionButtons.jsx
@@ -0,0 +1,12 @@
+const ActionButtons = () => {
+  return (
+    <div className="w-full flex gap-4 mt-4">
+      <button className="w-full bg-indigo-600 hover:bg-indigo-800">
+        Interested
+      </button>
+      <button className="w-full">Going</button>
+    </div>
+  );
+};
+
+export default ActionButtons;
--- components/events/EventCard.jsx
+++ components/events/EventCard.jsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
+import ActionButtons from "./ActionButtons";
 
 export default function EventCard({ event }) {
   return (
     <div className="overflow-hidden rounded-md bg-[#242526]">
       <Image
         src={event?.imageUrl}
-        alt="Event 1"
-        className="w-full"
-        width={500}
-        height={500}
+        alt={event?.name}
+        className="w-full h-[300px]"
+        width={200}
+        height={300}
       />
       <div className="p-3">
         <Link href={`/event/${event._id}`} className="font-bold text-lg">
@@ -17,17 +18,12 @@ export default function EventCard({ event }) {
         </Link>
         <p className="text-[#9C9C9C] text-sm mt-1">{event?.location}</p>
         <div className="text-[#737373] text-sm mt-1">
-          <span> {event.interested_ids.length} Interested</span>
-          <span>|</span>
+          <span className="me-2">{event.interested_ids.length} Interested</span>
+          <span className="me-2">|</span>
           <span>{event.going_ids.length} Going</span>
         </div>
 
-        <div className="w-full flex gap-4 mt-4">
-          <button className="w-full bg-indigo-600 hover:bg-indigo-800">
-            Interested
-          </button>
-          <button className="w-full">Going</button>
-        </div>
+        <ActionButtons />
       </div>
     </div>
   );
